40d57a1fd8150dd328752058162a2eacb58687ef
Eric França <ericprogramacao96@gmail.com>
2023-06-03 00:53:52 -0300

    Classes e métodos de forma fácil

--- src/teste_curso_games/Game.java
+++ src/teste_curso_games/Game.java
@@ -4,19 +4,17 @@ public class Game {
 
 	public static void main(String[] args) {
 		
-		//int contador = 0;
-		/*
-		while(contador < 10) {
-			
-			System.out.println("O número atual e: "+ contador);
-			
-			contador ++;
-		}
-		*/
+		Player player = new Player();
 		
-		for(int i = 2; i <= 10; i+=2) {
-			System.out.println("O número atual e: "+ i);
-		}
+		player.perderVida();
+		
+		player.perderVida();
+		
+		player.ganharVida();
+		
+		System.out.println(player.vida);
+		
+		
 		
 		
 	}
