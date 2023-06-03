3598b0241cbee71c527bef703c68fee15085401d
Eric França <ericprogramacao96@gmail.com>
2023-06-03 01:35:12 -0300

    Lista em Java

--- src/teste_curso_games/Game.java
+++ src/teste_curso_games/Game.java
@@ -1,15 +1,31 @@
 package teste_curso_games;
 
+import java.util.ArrayList;
+
 public class Game {
 
 	public static void main(String[] args) {
 		
 		Player player = new Player();
-		Elfinimigo elf = new Elfinimigo(10);
+		ArrayList<Inimigo> inimigos = new ArrayList<Inimigo>();
 		
-		elf.tomarDano();
 		
-		System.out.println(elf.getVida());
+		inimigos.add(new Elfinimigo(100));
+		inimigos.add(new Elfinimigo(100));
+		inimigos.add(new Elfinimigo(100));
+		inimigos.add(new Elfinimigo(100));
+		inimigos.add(new Elfinimigo(100));
+		
+		
+		for(int i = 0; i < inimigos.size(); i++) {
+			Inimigo inimigoLocal = inimigos.get(i);
+			if(i == 1) {
+				inimigoLocal.tomarDano();
+				System.out.println(inimigoLocal.vida);
+			}else {
+				System.out.println(inimigoLocal.vida);
+			}
+		}
 		
 	}
 	
